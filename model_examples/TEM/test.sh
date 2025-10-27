d6a8e8f5d2f1e67ddf11ecdef0d9c6c1e2720dd3
Tobey Carman <tcarman2@alaska.edu>
2025-10-27 12:13:37 -0800

    Fix tem test script to mesh with recent updates to install layout, etc

--- model_examples/TEM/test.sh
+++ model_examples/TEM/test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -f
 
 # Make a place to save our output...
-mkdir runs
+# Would put this in actual /tmp, but in some (many?) cases it leads
+# to "no space left on device" errors...
+mkdir -p tmp/tem_test_run 
 
 # Use the helper tool to setup a working directory for your experiment
 # has copies of parameters, config files, and 
-pyddt-swd --input-data-path ${HOME}/install_dvmdostem/demo-data/cru-ts40_ar5_rcp85_ncar-ccsm4_toolik_field_station_10x10/ runs/EE3_warming
+pyddt-swd --force --input-data-path /opt/dvmdostem/demo-data/cru-ts40_ar5_rcp85_ncar-ccsm4_toolik_field_station_10x10/ tmp/tem_test_run/EE1_warming
 
 # Fiddle with the settings...single pixel, turn on some output variables...
-cd runs/EE3_warming/
-pyddt-runmask --reset --yx 5 5
+cd tmp/tem_test_run/EE1_warming/
+pyddt-runmask --reset --yx 5 5 run-mask.nc
 pyddt-runmask --reset --yx 5 5 run-mask.nc 
 
 pyddt-outspec config/output_spec.csv --on LAYERDZ m l
@@ -19,7 +21,7 @@ pyddt-outspec config/output_spec.csv --on TLAYER m l
 pyddt-outspec -s config/output_spec.csv 
 
 # Run the model
-dvmdostem -p 25 -e 25 -s 15 -t 115 -n 85
+dvmdostem -p 25 -e 25 -s 15 -t 25 -n 0
 
 # outputs are in the output folder!
 ls output/
