a4844248f2cd04b7b1eaad400fb425ac98d1afce
Rich Fiorella <rfiorella@lanl.gov>
2025-12-01 16:21:57 -0700

    update inputdata script to make sure paths exist

--- tools/scripts/get_inputdata.sh
+++ tools/scripts/get_inputdata.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -f
 
-git clone --depth=1 --single-branch -b main https://github.com/ngee-arctic/field-to-model-inputdata /mnt/inputdata
+git clone --depth=1 --single-branch -b main https://github.com/ngee-arctic/field-to-model-inputdata /mnt/inputdata || true
+
+# make necessary folders in output volume
+mkdir -p /mnt/output/cime_case_dirs
+mkdir -p /mnt/output/cime_run_dirs
