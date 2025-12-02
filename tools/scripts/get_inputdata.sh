5efdea27678d6a9c4a99f416734a6c00934de0c2
Rich Fiorella <rfiorella@lanl.gov>
2025-12-02 11:57:11 -0700

    unzip inputdata by default

--- tools/scripts/get_inputdata.sh
+++ tools/scripts/get_inputdata.sh
@@ -2,6 +2,9 @@
 
 git clone --depth=1 --single-branch -b main https://github.com/ngee-arctic/field-to-model-inputdata /mnt/inputdata || true
 
+# unpack zipped data
+find /mnt/inputdata -name '*.gz' -exec gunzip -v {} +
+
 # make necessary folders in output volume
 mkdir -p /mnt/output/cime_case_dirs
 mkdir -p /mnt/output/cime_run_dirs
